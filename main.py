6413ef805fbc566f90c648e7551de377783f315c
Mosah04 <140697359+Mosah04@users.noreply.github.com>
2024-05-15 11:29:13 +0100

    changes2

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import streamlit as st
 
-st.markdown(r"<h1 style={text-align:center} >LOLACAP</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align:center;' >LOLACAP</h1>", unsafe_allow_html=True)
+st.markdown("""<style>
+            .st-emotion-cache-1cypcdb.eczjsme11{
+                visibility: hidden;
+            }
+            </style>""", unsafe_allow_html=True)
 
 st.write("MP4, MOV, M4A, AVI for video Maximum 200Mo")
 
-is_video_uploaded = False
+st.session_state.is_video_uploaded = False
 
 def onVideoChange():
-    global is_video_uploaded
     if st.session_state.file_uploaded_video is not None:
-        is_video_uploaded = True
+        st.session_state.is_video_uploaded = True
 
-if is_video_uploaded == False :
-    video_uploaded = st.file_uploader("Upload a video for obtaining subtitles in Yoruba or Fon", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
-    with st.form("Form 2", border=False):
-        st.text_input("Video URL", placeholder="Paste a Youtube URL")
-        st.form_submit_button("Load")
-else:
-    st.video("input.mp4")
+
+video_uploaded = st.file_uploader("Upload a video for obtaining subtitles in Yoruba or Fon", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
+if video_uploaded:
+     st.video(video_uploaded)
+with st.form("Form 2", border=False):
+    st.text_input("Video URL", placeholder="Paste a Youtube URL", key="youtube_url")
+    s_state = st.form_submit_button("Load")
+if s_state:
+    if st.session_state.youtube_url == "":
+        st.warning("Please fill the above fields.")
+    else:
+        st.success("Submitted successfully")
