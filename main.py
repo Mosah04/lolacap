56e3faecb5003d3340cb558d1dfec0eeaf4885d6
Mosah04 <140697359+Mosah04@users.noreply.github.com>
2024-05-15 09:19:49 +0100

    changes

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ st.write("MP4, MOV, M4A, AVI for video Maximum 200Mo")
 
 is_video_uploded = False
 
+def onVideoChange():
+    # if st.session_state.file_uploaded_video:
+    pass
+
 if is_video_uploded == False :
-    video_uploded = st.file_uploader("Upload a file for obtaining subtitles", type=('MP4', 'MOV', 'M4A', 'AVI'))
+    video_uploaded = st.file_uploader("Upload a file for obtaining subtitles", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
     with st.form("Form 2", border=False):
         st.text_input("Video URL", placeholder="Paste a Youtube URL")
         st.form_submit_button("Load")
