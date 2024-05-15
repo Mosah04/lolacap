183b56151aa4390822b7fa0cb32c6cc6e3b1cbfd
Mosah04 <140697359+Mosah04@users.noreply.github.com>
2024-05-15 09:39:40 +0100

    changes1

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ st.markdown(r"<h1 style={text-align:center} >LOLACAP</h1>", unsafe_allow_html=Tr
 
 st.write("MP4, MOV, M4A, AVI for video Maximum 200Mo")
 
-is_video_uploded = False
+is_video_uploaded = False
 
 def onVideoChange():
-    # if st.session_state.file_uploaded_video:
-    pass
+    global is_video_uploaded
+    if st.session_state.file_uploaded_video is not None:
+        is_video_uploaded = True
 
-if is_video_uploded == False :
-    video_uploaded = st.file_uploader("Upload a file for obtaining subtitles", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
+if is_video_uploaded == False :
+    video_uploaded = st.file_uploader("Upload a video for obtaining subtitles in Yoruba or Fon", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
     with st.form("Form 2", border=False):
         st.text_input("Video URL", placeholder="Paste a Youtube URL")
         st.form_submit_button("Load")
