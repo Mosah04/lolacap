5f23e903324adf8e336c0c2b25b0004e8898212c
Mosah04 <140697359+Mosah04@users.noreply.github.com>
2024-05-16 05:25:26 +0100

    changes

--- main.py
+++ main.py
@@ -91,8 +91,10 @@ if st.session_state.add_sub:
             output_lang = "fon"
         hardcoded = st.session_state.output_lang_select == "Hardcoded"
         video = doSub.run(output_lang, hardcoded)
-        output_placeholder.video(video)
-        output_placeholder.download_button('Download subtitled video', video, file_name="LolacapSub")
+        out_container = output_placeholder.container()
+        with out_container:
+            st.video(video)
+            st.download_button('Download subtitled video', video, file_name="LolacapSub.mp4")
     doRun()
     
 # video_uploaded = st.file_uploader("Upload a video for obtaining subtitles in Yoruba or Fon", type=('MP4', 'MOV', 'M4A', 'AVI'), key="file_uploaded_video", on_change=onVideoChange)
